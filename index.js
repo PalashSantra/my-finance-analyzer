3570ebf99e8d08a966e7caf444c5adeae8edd75c
Palash Santra <santra.mic.palash@gmail.com>
2022-10-29 20:05:19 +0530

    integration with angular

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv')
 const route = require('./router/route')
 const initializeDB = require('./db/connection')
+const path = require('path')
 
 // Environment Config
-//dotenv.config({path:'./config.env',override:true})
+// Not required for deployment
+dotenv.config({path:'./config.env',override:true})
 
 //Initializing DB
 require('./db/connection')
@@ -12,6 +14,25 @@ initializeDB().then(()=>{}).catch(()=>{})
 //Configure Express
 const app = express()
 app.use(express.json()).use(route)
+app.use('/', express.static(path.join(__dirname, './dist/my-finance-analyzer-angular')))
+app.use((req, res, next) => {
+    let url
+    if(process.env.PROFILE && process.env.PROFILE==='DEV')
+        url='http://localhost:4200'
+    else if(process.env.PROFILE && process.env.PROFILE==='PROD')
+        url=process.env.PROD_URL
+    if(url){
+        res.setHeader('Access-Control-Allow-Origin', url);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    next();
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/my-finance-analyzer-angular/index.html'));
+});
 app.listen(process.env.PORT, async ()=>{
     console.log(`Server is running at port : ${process.env.PORT}`);
 })
